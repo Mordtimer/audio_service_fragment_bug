bc86fa16eecd1f6b1c0a6354278024d6bead55ce
Filip Gabryszewski <f.gabryszewski@mobitouch.net>
2022-10-05 19:09:20 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/audio_service_fragment_bug/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.audio_service_fragment_bug
+
+import com.ryanheise.audioservice.AudioServiceActivity
+import com.ryanheise.audioservice.AudioServiceFragmentActivity
+
+
+class MainActivity : AudioServiceActivity() {
+}
+
+class MainActivity2 : AudioServiceFragmentActivity() {
+}
